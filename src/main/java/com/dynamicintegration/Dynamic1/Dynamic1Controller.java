94f37344af5b6137fa37ead6a2b341f8207f0c22
Khalil Virji <khalil_virji@hotmail.com>
2020-11-06 12:43:46 -0500

    added http header validation

--- src/main/java/com/dynamicintegration/Dynamic1/Dynamic1Controller.java
+++ src/main/java/com/dynamicintegration/Dynamic1/Dynamic1Controller.java
@@ -7,14 +7,21 @@ import java.util.Map;
 @RestController
 public class Dynamic1Controller {
     private static D1ReaderController d1ReaderController = D1ReaderController.getInstance();
+    private static final String authkey = "securekey";
+    private static final String authpswd = "securepswd";
 
-    @RequestMapping(method= RequestMethod.GET, value="/")
-    public String homepage() {
-        return "HELLO WORLD";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String homepage(@RequestHeader Map<String, String> headers) {
+        if (authenticate(headers)) {
+            return "HELLO secure user";
+        } else {
+            return "Hello unsecure user";
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/startReading")
-    public void startRead(@RequestHeader Map<String, String> headers){
+    public void startRead(@RequestHeader Map<String, String> headers) {
         //validate request
         String name = headers.get("rname");
         D1Reader d1Reader = d1ReaderController.getReader(name);
@@ -22,10 +29,18 @@ public class Dynamic1Controller {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/stopReading")
-    public void stopRead(@RequestHeader Map<String, String> headers){
+    public void stopRead(@RequestHeader Map<String, String> headers) {
         //validate request
         String name = headers.get("rname");
         D1Reader d1Reader = d1ReaderController.getReader(name);
         d1Reader.stopReading();
+    }
+
+    private Boolean authenticate(Map<String, String> headers) {
+        if (headers.containsKey("authkey") && headers.containsKey("authpswd") && headers.get("authkey").equals(authkey) && headers.get("authpswd").equals(authpswd)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
